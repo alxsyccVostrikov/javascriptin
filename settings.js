0fabe19c92a9beb69a2792f9c0c88df89552ab5c
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2024-10-26 11:11:24 -0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,53 @@
+/*function file()
+{
+
+  let filename="settings.js"
+  document.write("from '"+filename+"'");
+}*/
+
+
+icon=[];
+uri=[];
+field=["email","password"];
+page=
+[
+  ["index.php","Initial"],
+  ["sobre.php","About"],
+  ["contato.php","Contact"],
+  ["QuemSomos.php","WhoWeAre"],
+  ["denuncia.php","report"],
+  ["contribuir.php","donate"],
+];
+glyphicon=
+[
+  ["signup.php","user","sign-in"],
+  ["signin.php","log-in","log-in"],
+];
+
+btn_su=["sign-up","success"];
+webpage="alx";
+person=
+{
+  user:"alx",
+  email:"alx",
+};
+
+
+ url=[
+  "https://www.facebook.com/"+webPage,
+  "https://www.instagram.com/"+webPage,
+  "https://mail.yandex.com/?uid=869376094#compose",
+  "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to="+person["email"]+"&su=DenunciaAnimale&body=my-text&ui=2&tf=1&pli=1",
+  "https://web.telegram.org",
+  "https://www.skype.com",
+  "https://chat.whatsapp.com/a"+webPage,
+  "https://www.patreon.com/"+webPage,
+  "https://www.bitcoin.com/",
+  "https://www.paypal.com/"+webPage,
+  "https://www.mercadopago.com.br/"+webPage,
+  "https://pagseguro.uol.com.br/"+webPage,
+  "https://www.visa.com/"+webPage,
+  "https://www.bradesco.com/"+webPage,
+  "https://www.itau.com/"+webPage,
+  "http://www.twitter.com/"+webPage
+  ];
