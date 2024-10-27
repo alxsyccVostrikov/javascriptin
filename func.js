8abc8cdf6a182a76f020939a2a7ce95b037e03a6
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2024-10-27 19:07:16 -0300

    Update func.js

--- func.js
+++ func.js
@@ -4,6 +4,28 @@ function file()
   document.write("from ''"+filename+"<br>");
 } 
 
+function boobleSort(array) 
+{
+  let temp=""; 
+  
+  for(i=0;i<array.length;i++)
+  {
+    for(j=0;j<array.length;j++)
+    {
+      if(array[j]>array[i])
+      {
+        document.write(array[j]+" bigger than "+array[i]+"<br>")
+        temp=array[j];
+        array[j]=array[i];
+        array[i]=temp;
+        document.write(...array);
+      }
+        
+    }
+  }
+  return array;
+}
+
 function tab(times=1)
 {
 	let t=" ";
