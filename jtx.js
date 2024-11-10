04620bc5e43fa5a06ef4c9463e100c2921f57887
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2024-11-10 14:33:03 -0300

    Add files via upload

--- jtx.js
+++ jtx.js
@@ -1,23 +1,4 @@
-function init(lang)
-{
-
-	let row=20;
-	let cols=50;
-	document.write("<h1>jTx</h1>"+
-	"<h3>paste your code bellow and switch what lang's gonna convert to</h3>"+
-	"<textarea id='codejtxFROM' rows='"+row+"' cols='"+cols+"' placeholder='JS code here...'>"+
-	"</textarea>"+
-	"<textarea id='codejtxTO' rows='"+row+"' cols='"+cols+"' placeholder='"+lang+" Code here...' readonly>"+
-	"</textarea>"+
-	"<br>"+
-	"<button onclick=jtx('"+lang+"')>convert TO:"+lang+"</button>"
-	);
-}
-
-function jtx(lang) 
-{
-	let newS=document.getElementById("codejtxFROM").value;
-	let tag=
+let tag=
 	{
 		"js":[/document.write/gi,/let/gi,/var/gi,/function/],
 		"bash":["echo"],
@@ -34,6 +15,64 @@ function jtx(lang)
 
 
 	};
+
+	
+let link={
+		"php":["https://www.writephponline.com/","https://onecompiler.com/php/42x5ab9pq","https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler"],
+		"java":["https://replit.com/languages/java10","https://onecompiler.com/java","https://www.onlinegdb.com/online_java_compiler","https://www.w3schools.com/java/tryjava.asp?filename=demo_compiler"],
+	};
+
+let lang="";
+
+function optionLang_function()
+ {
+
+	lang=document.getElementById('optionLang').value;
+	document.getElementById('btn_Lang').innerHTML="convert TO:"+lang;
+	document.getElementById('h2_lang').innerHTML="Try "+lang+" compiler";
+	const div = document.querySelector("h4");
+	div.innerHTML ="";
+		
+	for(let i=0;i<link[lang].length;++i)
+	{
+		div.innerHTML+="<button>"+"<a href='"+link[lang][i]+"' style='text-decoration:none;text-font:Arial;' target='_blank' id=href_"+i+"> "+link[lang][i]+"</a>"+"</button>"+"<br>";
+	}
+
+}
+function init(chosen=lang)
+{
+	let row=20;
+	let cols=50;
+	document.write("<h1>jTx</h1>"+
+	"<h3>paste your code bellow and switch what lang's gonna convert to<select id='optionLang' onchange='optionLang_function()'");
+	for(let i in tag)
+	{
+		if(i==chosen)
+		{
+			document.write("<option id='"+i+"_opt' selected>"+i+"</option>");
+		}
+		else
+		{
+			document.write("<option id='"+i+"_opt'>"+i+"</option>");
+		}
+	}
+	document.write("</select></h3>"+
+	"<textarea id='codejtxFROM' rows='"+row+"' cols='"+cols+"' placeholder='JS code here...'>"+
+	"</textarea>"+
+	"<textarea id='codejtxTO' rows='"+row+"' cols='"+cols+"' placeholder='Coverted code here...' readonly>"+
+	"</textarea>"+
+	"<br>"+
+	"<button onclick=jtx('"+chosen+"') id='btn_Lang'>convert TO:"+chosen+"</button>"
+	);
+}
+
+
+
+
+function jtx(lang) 
+{
+	let newS=document.getElementById("codejtxFROM").value;
+	
 	let tgs=Object.keys(tag).length;
 	for(let i=0;i<tag[lang].length;++i)
 	{
@@ -44,21 +83,18 @@ function jtx(lang)
 }
 
 
-function button(argument)
- {
-	link=argument;
-	for(let i=0;i<link.length;++i)
-		document.write("<br>"+"<button>"+"<a href='"+link[i]+"' style='text-decoration:none' target='_blank'> "+link[i]+"</a>"+"</button>");
-}
 
-
-function compiler(lang) 
+function compiler(chosen=lang) 
 {
-	let link={
-		"php":["https://www.writephponline.com/","https://onecompiler.com/php/42x5ab9pq","https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler"],
-		"java":["https://replit.com/languages/java10","https://onecompiler.com/java","https://www.onlinegdb.com/online_java_compiler","https://www.w3schools.com/java/tryjava.asp?filename=demo_compiler"],
-	};
-	document.write("<h2>"+"Try "+lang+" Compiler");
-	for(let i=0;i<link[lang].length;++i)
-		document.write("<br>"+"<button>"+"<a href='"+link[lang][i]+"' style='text-decoration:none;text-font:Arial;' target='_blank'> "+link[lang][i]+"</a>"+"</button>");
+	let string="";
+	string+="<h2 id='h2_lang'>"+"Try "+chosen+" Compiler";
+	string+="<h4 id='h2'>";
+	for(let i=0;i<link[chosen].length;++i)
+	{
+		string+="<button>"+"<a href='"+link[chosen][i]+"' style='text-decoration:none;text-font:Arial;' target='_blank' id=href_"+i+"> "+link[chosen][i]+"</a>"+"</button>"+"<br>";
+	}
+	string+="</h4>";
+	document.write(string);
+
+
 }
