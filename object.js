9a74e78410dd9e9d5916a7fdfd45ede545cc6fa5
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2024-11-15 13:22:17 -0300

    Update object.js

--- object.js
+++ object.js
@@ -1,8 +1,3 @@
-function file()
-{
-  let filename="object.js";
-  document.write("from ''"+filename+"<br>");
-} 
 
 function lengthFromObject(obj)
 {
@@ -18,4 +13,45 @@ function lengthFromObject(obj)
 function getSizeObj(obj)
 {
 	return Object.keys(obj).length;
+}
+
+
+function getFields(obj)
+{
+	let ar=[];
+	for(let i in obj)
+	{
+		ar.push(i);
+	}
+	return ar;
+}
+
+function getFieldFromIndex(obj,position)
+{
+	let index=0;
+	let ar="";
+	for(let i in obj)
+	{
+		if(index==position)
+		{
+			return i;
+		}
+		++index;
+	}
+
+}
+
+
+function getValueFromIndex(obj,position)
+{
+	let index=0;
+	let ar="";
+	for(let i in obj)
+	{
+		if(index==position)
+		{
+			return obj[i];
+		}
+		++index;
+	}
 }
