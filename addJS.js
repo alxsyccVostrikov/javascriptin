ce59d3c98855cd3161175fbb75b8bee3e61abbc5
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2024-12-15 09:35:39 -0300

    Add files via upload

--- addJS.js
+++ addJS.js
@@ -1,20 +1,9 @@
-/*
----
----  print files they are gonna be imported
----
-*/
+function isitWorkin(file)
+{
 
-/*
-Code...
- let files=["printf.js","cpt.js"];
- printFiles(files);
-*/
+ // return if(file==true);
+}
 
-/*
-result...
-printf.js
-cpt.js
-*/
 
 function printFiles(array)
 {
@@ -27,27 +16,6 @@ function printFiles(array)
 }
 
 
-/*
------------------------------------------------------------------------------------------------
---- add files from a obj
----
-*/
-
-/*
-Code
- let files={
-          "printf.js":"",
-          "cpt.js":"",
-          };
-
-         addFromObject(files);
-*/
-
-/*
-result...
- document.write("<script type='text/javascript' src='printf.js'><\/script>");
- document.write("<script type='text/javascript' src='cpt.js'><\/script>");
-*/
 function addFromObject(obj)
 {
   
@@ -58,28 +26,6 @@ function addFromObject(obj)
 
 }
 
-/*
------------------------------------------------------------------------------------------------
---- add files from an array
----
-*/
-
-/*
-Code
- let files=[
-          "printf.js",
-          "cpt.js",
-          ];
-
-         addFromObject(files);
-*/
-
-/*
-result...
- document.write("<script type='text/javascript' src='printf.js'><\/script>");
- document.write("<script type='text/javascript' src='cpt.js'><\/script>");
-
-*/
 
 function addJSFromArray(array,path="./")
 {
@@ -92,23 +38,6 @@ function addJSFromArray(array,path="./")
   
 }
 
-/*
------------------------------------------------------------------------------------------------
---- add files from params
----
-*/
-
-/*
-Code...
-  addJSFromParam("printf.js","cpt.js");
-*/
-
-/*
-result...
- document.write("<script type='text/javascript' src='printf.js'><\/script>");
- document.write("<script type='text/javascript' src='cpt.js'><\/script>");
-
-*/
 
 function addJSFromParam(...params)
 {
@@ -119,110 +48,18 @@ for(let i=0;i<params.length;i++)
   
 }
 
-/*
------------------------------------------------------------------------------------------------
---- add single files each one
----
-*/
 
-/*
-Code...
-  let file01="cpt.js";
-  let file02="printf.js";
-  addSingleJSF(file01);
-  addSingleJSF(file02);
-*/
-
-/*
-result...
- document.write("<script type='text/javascript' src='printf.js'><\/script>");
- document.write("<script type='text/javascript' src='cpt.js'><\/script>");
-
-*/
-
-function addSingleJSF(file)
+function addJs(file)
 {
     document.write("<script type=\"text/javascript\" src=\""+file+"\"><\/script>");
 
   
 }
 
-/*
------------------------------------------------------------------------------------------------
---- add files undepends on which type of var is..obj,array or single file
----
-*/
 
-/*
-Code...
-  let file01="cpt.js";
-  let file02="printf.js";
-  add(file01);
-  add(file02);
-
-  
-  let f1=["cpt.js","printf.js"];
-  add(f1);
-
-  let f2={"cpt.js":"","printf.js":"",};
-  add(f2)
-
-
-  add("cpt.js","printf.js");
-*/
-
-/*
-result...
- document.write("<script type='text/javascript' src='printf.js'><\/script>");
- document.write("<script type='text/javascript' src='cpt.js'><\/script>");
-
-*/
-
-function add(...argument)
+function addCss(file)
 {
-
-  let x=typeof(argument);
-  let y=Object.keys(argument).length;
-  if(y==1)
-  {
-    let index=0;
-    if(argument[index]=="[object Object]")
-    {
-      let nobj=Object.keys(argument[index]);
-      for(let i in nobj)
-      {
-        document.write("<script type='text/javascript' src='"+nobj[i]+"'><\/script>");
-      }
-    }
-    else
-    {
-      let nobj=argument[index];
-      let unknow=argument[index][index].length;
-      if(unknow==1)
-      {
-        for(let i in argument)
-        {
-          document.write("<script type='text/javascript' src='"+argument[i]+"'><\/script>");
-        }
-      
-      }
-      else
-      {
-        for(let i in nobj)
-        {
-          document.write("<script type='text/javascript' src='"+nobj[i]+"'><\/script>");
-        }
-      }
-
-
-    }
-  }
-  
-  else
-  {
-    for(let i in argument)
-      document.write("<script type='text/javascript' src='"+argument[i]+"'><\/script>");
-  }
+    document.write("<link rel='stylesheet' type='text/css' href='"+file+"'>");
 
   
 }
